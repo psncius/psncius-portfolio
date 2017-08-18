110671f0050cbc6c40a97efed14fe633792a2cc0
fsncius <penciusms43@gmail.com>
2017-08-18 19:12:46 +0700

    fix-hot-loader

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -13,7 +13,7 @@ export default class Home extends Component {
     return (
       <Container>
         <h1>Hello</h1>
-        <Image src={require('../assets/hero.gif')} alt="hero-image"/>
+        <Image src={require('../assets/profile.jpg')} alt="fensius-image"/>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce in velit dolor. In et dolor non sapien ultricies hendrerit. Maecenas ultricies porttitor est, sit amet eleifend sem egestas ut. Cras eget imperdiet est. Cras viverra ligula urna, eget rutrum erat lacinia nec. Praesent nec libero accumsan turpis molestie dapibus. Mauris scelerisque ligula sed felis ultricies, ut efficitur orci dignissim. Nam eleifend ligula sit amet congue dapibus. Vestibulum ut nisi vel quam luctus mollis vel ac mi. Vivamus pellentesque finibus sapien quis convallis. Donec at interdum mi. Sed finibus sed justo ac malesuada. Aliquam pulvinar pellentesque lacinia. Integer commodo blandit quam, ut blandit quam pretium eget.
           </p>
