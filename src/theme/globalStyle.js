2a2c40b7837299a7b3d8a303be208021b1a9a09b
fsncius <penciusms43@gmail.com>
2017-08-18 17:30:32 +0700

    firs portfolio

---
+++ src/theme/globalStyle.js
@@ -0,0 +1,10 @@
+import {injectGlobal} from 'styled-components';
+/* eslint-disable */
+injectGlobal`
+  @import url('https://fonts.googleapis.com/css?family=Montserrat');
+
+  body {
+    font-family: 'Montserrat', sans-serif;
+    font-size: 24px;
+  }
+`
