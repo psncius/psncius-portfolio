bfab0dbdccee3bc7de9b3bd7ef3d50b343363049
fsncius <penciusms43@gmail.com>
2017-08-21 13:58:38 +0700

    fix-webpack

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,10 +1,11 @@
 import React, { Component, PropTypes } from 'react';
-import {Container} from '../../theme/grid';
+import {Container} from 'theme/grid';
 import {
   Image,
   FensiusImage,
   RevealP
 } from './Home.style';
+import WhenInView from 'components/WhenInView/WhenInView';
 
 export default class Home extends Component {
   static propTypes = {};
@@ -16,9 +17,13 @@ export default class Home extends Component {
             <h1>Fensius Musa Aritonang</h1>
             <h2>Front-end Developer</h2>
           </FensiusImage>
-        <RevealP >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce in velit dolor. In et dolor non sapien ultricies hendrerit. Maecenas ultricies porttitor est, sit amet eleifend sem egestas ut. Cras eget imperdiet est. Cras viverra ligula urna, eget rutrum erat lacinia nec. Praesent nec libero accumsan turpis molestie dapibus. Mauris scelerisque ligula sed felis ultricies, ut efficitur orci dignissim. Nam eleifend ligula sit amet congue dapibus. Vestibulum ut nisi vel quam luctus mollis vel ac mi. Vivamus pellentesque finibus sapien quis convallis. Donec at interdum mi. Sed finibus sed justo ac malesuada. Aliquam pulvinar pellentesque lacinia. Integer commodo blandit quam, ut blandit quam pretium eget.
-          </RevealP>
+          <WhenInView>
+            {({isInView}) =>
+            <RevealP hide={!isInView}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce in velit dolor. In et dolor non sapien ultricies hendrerit. Maecenas ultricies porttitor est, sit amet eleifend sem egestas ut. Cras eget imperdiet est. Cras viverra ligula urna, eget rutrum erat lacinia nec. Praesent nec libero accumsan turpis molestie dapibus. Mauris scelerisque ligula sed felis ultricies, ut efficitur orci dignissim. Nam eleifend ligula sit amet congue dapibus. Vestibulum ut nisi vel quam luctus mollis vel ac mi. Vivamus pellentesque finibus sapien quis convallis. Donec at interdum mi. Sed finibus sed justo ac malesuada. Aliquam pulvinar pellentesque lacinia. Integer commodo blandit quam, ut blandit quam pretium eget.
+            </RevealP>
+          }
+          </WhenInView>
       </Container>
     );
   }
